c06408fe58dd3feac827b8453dc317de8b9104c1
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-10-04 00:10:10 +0300

    test

---
+++ src/FormSuccess.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FormSuccess = () => {
+  return (
+    <div>
+      <div className="form-content-right">
+      <div className="form-success">
+        We have received
+      </div>
+      <img src="img/img-3.svg" alt="success-image" className="form-img-2"/>
+      </div>
+    </div>
+  )
+}
+
+export default FormSuccess
+
